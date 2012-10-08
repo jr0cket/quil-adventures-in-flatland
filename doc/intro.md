7088d4cfa2c0ea63e004b20f6ab685242903e517
John Stevenson <john@jr0cket.co.uk>
2012-10-08 15:44:47 +0100

    Project created with lein new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to adventures-in-flatland
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/adventures_in_flatland/core_test.clj
@@ -0,0 +1,7 @@
+(ns adventures-in-flatland.core-test
+  (:use clojure.test
+        adventures-in-flatland.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
